5fe74a0f99f43601fe00386a00ce8c31da4b9ae8
Joe Engalan <joe.engalan@gmail.com>
2016-06-16 04:08:59 +0000

    Finished toRoman conversion

--- toroman-tests.c
+++ toroman-tests.c
@@ -7,6 +7,8 @@ toroman-tests.c
 
 char numeral[20];
 
+/* TO ROMAN TESTS */
+
 START_TEST(shouldConvert5)
 {
   toRoman(numeral, 5);
@@ -22,6 +24,33 @@ START_TEST(shouldConvert9)
   ck_assert_str_eq(numeral, "IX");
 }
 END_TEST
+
+START_TEST(shouldConvertTensPlace)
+{
+  toRoman(numeral, 56);
+
+  ck_assert_str_eq(numeral, "LVI");
+}
+END_TEST
+
+START_TEST(shouldInsertBlankForZeroDigits)
+{
+  toRoman(numeral, 2007);
+
+  ck_assert_str_eq(numeral, "MMVII");
+}
+END_TEST
+
+START_TEST(shouldReturnBlankIfNumberIsGreaterThan3999)
+{
+  int status = toRoman(numeral, 100000);
+
+  ck_assert_str_eq(numeral, "");
+  ck_assert_int_eq(status, 0);
+}
+END_TEST
+
+/* EXTRACTION TESTS */
 
 START_TEST(shouldExtractOnesDigit)
 {
@@ -67,6 +96,9 @@ int main(void)
 
   tcase_add_test(toRoman, shouldConvert5);
   tcase_add_test(toRoman, shouldConvert9);
+  tcase_add_test(toRoman, shouldConvertTensPlace);
+  tcase_add_test(toRoman, shouldInsertBlankForZeroDigits);
+  tcase_add_test(toRoman, shouldReturnBlankIfNumberIsGreaterThan3999);
 
   tcase_add_test(digitExtraction, shouldExtractOnesDigit);
   tcase_add_test(digitExtraction, shouldExtractTensDigit);
--- toroman.c
+++ toroman.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 
 static const char* ones[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+static const char* tens[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+static const char* hundreds[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+static const char* thousands[] = { "", "M", "MM", "MMM" };
 
 int extractDigit(int number, int place)
 {
@@ -12,7 +15,17 @@ int extractDigit(int number, int place)
 
 int toRoman(char* numeral, int number)
 {
+  if(number > 3999)
+  {
+    numeral[0] = 0;
+    return 0;
+  }
+
   int onesDigit = extractDigit(number, 1);
-  sprintf(numeral, "%s", ones[onesDigit]);
+  int tensDigit = extractDigit(number, 10);
+  int hundredsDigit = extractDigit(number, 100);
+  int thousandsDigit = extractDigit(number, 1000);
+
+  sprintf(numeral, "%s%s%s%s", thousands[thousandsDigit], hundreds[hundredsDigit], tens[tensDigit], ones[onesDigit]);
   return 1;
 }
